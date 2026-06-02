eb094deee82929b52d1f4279371f33b5f286015b
Gorbash Dev <benerd7villegas@gmail.com>
2026-06-02 09:49:05 -0400

    Inicio de mi App solo codec Creacion de login

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" contente="width=device-width, initial-scale=1.0">
+	<title>NOMINA APP</title>
+	<link rel="stylesheet" href="public/css/main.css">
+</head>
+<body>
+  <div class="container">
+    <div class="header">
+      	<h1>Aplicacion de Nomina solo Basic for all</h1>
+    </div>
+
+    <div class="login">
+      <form>
+	<div class="content-form">
+	  <input type="text" placeholder="Usuario:" class="input-login">
+	  <input type="text" placeholder="Clave:" class="input-login">
+	  <input type="submit" value="Acceder al Sistema" class="btn-form">
+	</div>
+ </form>
+    </div>
+    <div class="content">
+      Resumen de mi aplicacion 100% nativa lo hace todo sin hacer nada, este aplicacion fue hecha netamente basico para aprender a usar y dominar emacs, Si funciona de ayuda a otros espero que la disfruten.
+    </div>
+    <div class="footer">
+      <h4>Creado por Gorbash(SuhamyV)</h4>
+    </div>
+  </div>
+
+</body>
+</html>
