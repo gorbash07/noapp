2036caa5b25f59f96bc34258a96639b802b2c695
Gorbash Dev <benerd7villegas@gmail.com>
2026-06-06 23:50:51 -0400

    Index-home actualizado pendiente por redirigir botones

--- index.php
+++ index.php
@@ -5,27 +5,26 @@
 	<meta name="viewport" contente="width=device-width, initial-scale=1.0">
 	<title>NOMINA APP</title>
 	<link rel="stylesheet" href="public/css/main.css">
-</head>2
+</head>
 <body>
   <div class="container">
     <div class="header">
-      	<h1>Aplicacion de Nomina solo Basic for all</h1>
+      	<h1>SISTEMA DE NOMINA</h1>
     </div>
 
     <div class="login">
       <form>
 	<div class="content-form">
-	  <input type="text" placeholder="Usuario:" class="input-login">
-	  <input type="text" placeholder="Clave:" class="input-login">
-	  <input type="submit" value="Acceder al Sistema" class="btn-form">
+      <a href="#" class="btn btn-consulta">Consulta de nomina</a>
+      <a href="#" class="btn btn-acceso">Acceder al sistema</a>
 	</div>
  </form>
     </div>
     <div class="content">
-      Resumen de mi aplicacion 100% nativa lo hace todo sin hacer nada, este aplicacion fue hecha netamente basico para aprender a usar y dominar emacs, Si funciona de ayuda a otros espero que la disfruten.
+      Un sistema que puede ser ampleado
     </div>
     <div class="footer">
-      <h4>Creado por Gorbash(SuhamyV)</h4>
+      <h4>Creado por Gorbash(SuhamyV) y Colaboradores</h4>
     </div>
   </div>
 
