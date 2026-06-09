8995e558867d05fe1a43917e1ccd749dd0a3c2b8
kany07 <mendozajoskani@gmail.com>
2026-06-08 22:24:50 -0400

    vista edit y su controlador, actualizacion de la db

--- database/sistemadenomina.sql
+++ database/sistemadenomina.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 08, 2026 at 11:46 PM
+-- Generation Time: Jun 09, 2026 at 02:16 AM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.30
 
@@ -34,7 +34,7 @@ CREATE TABLE `empleados` (
   `nombres` varchar(100) NOT NULL,
   `apellidos` varchar(100) NOT NULL,
   `fecha_nacimiento` date DEFAULT NULL,
-  `sexo` enum('M','F','Otro') NOT NULL,
+  `sexo` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `estado_civil` varchar(50) DEFAULT NULL,
   `telefono` varchar(20) NOT NULL,
   `email` varchar(100) DEFAULT NULL,
@@ -48,8 +48,8 @@ CREATE TABLE `empleados` (
 --
 
 INSERT INTO `empleados` (`id_empleado`, `cedula`, `rif`, `nombres`, `apellidos`, `fecha_nacimiento`, `sexo`, `estado_civil`, `telefono`, `email`, `direccion`, `cargo`, `departamento`) VALUES
-(10, '12345', 'j-12345', 'Ejemplo1', 'ejemplo1', '2007-06-13', 'M', 'Concubinato', '12345', 'ejemplo1@gmail.com', 'Casa 1', 'Conserje', 'Limpieza'),
-(11, '67890', 'j-67890', 'Ejemplo2', 'ejemplo2', '2005-06-05', 'F', 'Casado', '67890', 'ejemplo2@gmail.com', 'Casa 2', 'Conserje', 'Limpieza'),
+(10, '12345', 'j-12345', 'Ejemplo1', 'ejemplo1', '2007-06-13', 'F', 'Concubinato', '12345', 'ejemplo1@gmail.com', 'Casa 1', 'Conserje', 'Limpieza'),
+(11, '67890', 'j-67890', 'Ejemplo2', 'ejemplo2', '2026-06-07', 'M', 'Viudo/a', '67890', 'ejemplo2@gmail.com', 'Casa 2', 'Conserje', 'Limpieza'),
 (12, '54321', 'j-54321', 'Ejemplo3', 'ejemplo3', '2000-01-20', 'M', 'Concubinato', '54321', 'ejemplo3@gmail.com', 'Casa 3', 'Conserje', 'Limpieza'),
 (13, '09876', 'j-09876', 'Ejemplo4', 'ejemplo4', '1996-07-26', 'F', 'Casado/a', '09876', 'ejemplo4@gmail.com', 'Casa 4', 'Conserje', 'Limpieza');
 
@@ -74,7 +74,7 @@ ALTER TABLE `empleados`
 -- AUTO_INCREMENT for table `empleados`
 --
 ALTER TABLE `empleados`
-  MODIFY `id_empleado` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_empleado` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
